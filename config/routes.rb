3b35934625d854a09c222f949c249708049c8f01
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-09-22 18:50:23 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'houses#index'
   
+  resources :houses, only: [:index]
   resources :abouts, only: [:index]
   resources :posts, only: [:index, :create]
 end
