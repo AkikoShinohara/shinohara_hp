6c529e4a832512e97510c66f8c8716b26ba9ee43
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-09-25 13:35:19 +0900

    お問い合わせ内容設定

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,10 +1,10 @@
 class InquiryMailer < ApplicationMailer
-  # def send_mail(inquiry)
-  #   @inquiry = inquiry
-  #   mail(
-  #     from: 'aaa@example.com',
-  #     to:   'my.rhythm.ism@gmail.com',
-  #     subject: 'お問い合わせ通知'
-  #   )
-  # end
+  def send_mail(inquiry)
+    @inquiry = inquiry
+    mail(
+      from: 'aaa@example.com',
+      to:   'my.rhythm.ism@gmail.com',
+      subject: 'お問い合わせ通知'
+    )
+  end
 end
