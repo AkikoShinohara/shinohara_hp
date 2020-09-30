a77a159d4d72e09b4d225ebd844e81c9543710d4
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-09-30 23:25:38 +0900

    controller update

--- app/controllers/constructions_controller.rb
+++ app/controllers/constructions_controller.rb
@@ -1,4 +1,18 @@
 class ConstructionsController < ApplicationController
   def index
+    @constructions = Construction.includes(:user).order("created_at DESC")
   end
+
+  def new
+    @construction = Construction.new
+  end
+
+  def create
+  end
+
+  private
+  def construction_params 
+    params.require(:construction).parmit(:image, :name, :message).merge(user_id: current_user.id)
+  end
+
 end
